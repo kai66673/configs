8efd1744504811a44c6fb9afb87773b91762bfb0
Alexander Kudryavtsev <kai66673@mail.ru>
2023-07-27 19:49:15 +0300

    ...

---
+++ nvim/init.lua
@@ -0,0 +1,12 @@
+-- Базовая конфигурация
+require('core.plugins')
+require('core.mappings')
+require('core.colors')
+require('core.configs')
+
+-- Конфигурация плагинов
+require('plugins.neo-tree')
+require('plugins.treesitter')
+require('plugins.lsp')
+require('plugins.mason')
+require('plugins.cmp')
---
+++ nvim/lua/core/mappings.lua
@@ -0,0 +1,6 @@
+vim.g.mapleader = " "
+
+-- neo-tree
+vim.keymap.set('n', '<leader>e', ':Neotree left toggle<CR>')
+vim.keymap.set('n', '<leader>o', ':Neotree left focus<CR>')
+vim.keymap.set('n', '<leader><s-e>', ':Neotree float toggle<CR>')
